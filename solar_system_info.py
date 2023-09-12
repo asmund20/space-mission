90e2dfffe33fbb98ca98a26077e025d9b339907b
asmund20 <asmund20@gmail.com>
2023-09-12 14:15:35 +0200

    printed some different stuff

--- solar_system_info.py
+++ solar_system_info.py
@@ -5,8 +5,10 @@ print('My system has a {:g} solar mass star with a radius of {:g} kilometers.'
       .format(system.star_mass, system.star_radius))
 
 for planet_idx in range(system.number_of_planets):
-    print('Planet {:d} is a {} planet with a semi-major axis of {:g} AU with mass {} solar masse and radius {} km'
-          .format(planet_idx, system.types[planet_idx], system.semi_major_axes[planet_idx], system.masses[planet_idx], system.radii[planet_idx]))
+    #print('Planet {:d} is a {} planet with a semi-major axis of {:g} AU with mass {} solar masse and radius {} km'
+          #.format(planet_idx, system.types[planet_idx], system.semi_major_axes[planet_idx], system.masses[planet_idx], system.radii[planet_idx]))
+
+    print(f"Planet {planet_idx} initial_angle: {system.initial_orbital_angles[planet_idx]:.2f}, aphelion angle: {system.aphelion_angles[planet_idx]:.2f}")
 
 print("info om solsystemet:")
 print(f"""
