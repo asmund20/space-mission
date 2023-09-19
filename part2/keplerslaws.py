6581e623d12426c11d0de79b4ce7cc4f48b7fdaf
felixmorland <felmorland@gmail.com>
2023-09-19 15:57:33 +0200

    Update keplerslaws.py

--- part2/keplerslaws.py
+++ part2/keplerslaws.py
@@ -13,23 +13,22 @@ a_zeron = system.semi_major_axes[0] # Store halvakse til hjemplaneten Zeron
 M_s = system.star_mass              # Massen til Stellaris Skarsgard
 M_zeron = system.masses[0]          # Massen til Zeron
 
+# Perioden til Zeron: bruker denne til a finne tiden det tar for 30 omlop rundt Stel. Skars.
+period_zeron = np.sqrt((4*np.pi**2 * a_zeron**3)/(cs.G_sol*(M_s+M_zeron)))
+dt = 1e-4
+t = np.arange(0, 30*period_zeron,dt)
+
 def periods(pos):
     periods = np.zeros(num_planets)
     for i in range(num_planets):
         x = pos[i,:,0]
         r = np.sqrt(pos[i,:,0]**2 + pos[i,:,1]**2)
-        index = np.argwhere(abs(x/r - np.cos(system.initial_orbital_angles[i])) <= 1e-3)
+        index = np.argwhere(abs(x/r - np.cos(system.initial_orbital_angles[i])) <= 1e-5)
     
         j = np.argwhere(index[1:,0]-index[:-1,0] != 1)
-        print(index[j[1]])
+        periods[i] = np.mean(index[j[0,0]:j[1,0]])*dt
+    return periods
 
-
-
-
-# Perioden til Zeron: bruker denne til a finne tiden det tar for 30 omlop rundt Stel. Skars.
-period_zeron = np.sqrt((4*np.pi**2 * a_zeron**3)/(cs.G_sol*(M_s+M_zeron)))
-dt = 1e-4
-t = np.arange(0, 30*period_zeron,dt)
 
 with open('positions.txt', 'r') as infile:
     lines = infile.readlines()
@@ -63,8 +62,11 @@ dist_ap = np.sum([np.linalg.norm(pos[0,i+1]-pos[0,i]) \
 vel_peri = dist_peri/(n*dt)     # Gjennomsnittsfart ved perihel
 vel_ap = dist_ap/(n*dt)         # Gjennomsnitssfart ved aphel
 
-periods(pos)
+num_periods = periods(pos)
+kepler_periods = [np.sqrt((4*np.pi**2 * system.semi_major_axes[i]**3)/(cs.G_sol*(M_s))) for i in range(num_planets)]
+newton_periods = [np.sqrt((4*np.pi**2 * system.semi_major_axes[i]**3)/(cs.G_sol*(M_s + system.masses[i]))) for i in range(num_planets)]
 
+print(num_periods, kepler_periods, newton_periods)
 print(
 f"""
 #=====================#
@@ -103,9 +105,9 @@ Diff:    v_p - v_a = {(vel_peri-vel_ap):g} AU/yr
 Perioden til Zeron:
 ========================================================
 Analytisk: P_an = {period_zeron} yr
-Numerisk:  P_num = {period_num} yr
+Numerisk:  P_num =  yr
 --------------------------------------------------------
-Diff:      P_an - P_num = {period_zeron-period_num:g} yr
+Diff:      P_an - P_num = {period_zeron:g} yr
 """
 )
 
