ea742f98a1c3abab130efc0fc46a36da9d76dc71
asmund20 <asmund20@gmail.com>
2023-09-27 21:57:53 +0200

    commented for part 3

--- part3/habitable_zone.py
+++ part3/habitable_zone.py
@@ -5,6 +5,7 @@ import numpy as np
 from plotting_orbits import main as plot_orbits
 
 # r i astronomiske enheter
+# utledet i artikkel 3
 def temp(r, s):
     return s.star_temperature*np.sqrt(s.star_radius*1e3/2/ut.AU_to_m(r))
 
@@ -14,25 +15,32 @@ def main():
 
     T = temp(system.semi_major_axes,system)
 
+    # printer info som vi har i tabell i rapport 3
     for i, t in enumerate(T):
         ikke = " ikke"*(not 260<t<390)
         print(f"Temperatur planet {i} har en overflatetemperatur på omtrent {t:.2f}. Den er{ikke} i habitable zone. {system.types[i]}")
 
+    # beregner overflatetemperaturen for en tenkt planet i bane med radius r
     r = np.linspace(0.0000000008,system.semi_major_axes[6], 1000)
     t = temp(r, system)
 
+    # finner indexene i r som tilsvarer planeter i den habitable sonen
     j = np.nonzero(260<t)
     i = np.nonzero(t[j] < 390)
 
+    # indre grense av den habitable sonen
     inner = r[i[0][0]]
+    # ytre grense av den habitable sonen
     outer = r[i[0][-1]]
 
     theta = np.linspace(0, 2*np.pi, 1000)
 
+    # plotter den habitable sonen som en grå disk
     plt.style.use("dark_background")
     plt.fill(outer*np.cos(theta),outer*np.sin(theta), alpha=0.2, color="white", label="habitable zone")
     plt.fill(inner*np.cos(theta),inner*np.sin(theta), color="black")
 
+    # plotter planetbanene oppå
     plot_orbits()
 
     plt.axis("equal")
--- part3/reference_frame.py
+++ part3/reference_frame.py
@@ -13,84 +13,102 @@ seed = 59529
 
 system = SolarSystem(seed)
 
-a_zeron = system.semi_major_axes[0] # Store halvakse til hjemplaneten Zeron
-M_s = system.star_mass              # Massen til Stellaris Skarsgard
-M_zeron = system.masses[0]          # Massen til Zeron
+#launch time relative to one period for Zeron
+# set a positive launch angle for something else than directly away from the sun
+def sim_launch_relative_period(nperiods, launch_angle=-1):
+    # Perioden til Zeron
+    a_zeron = system.semi_major_axes[0] # Store halvakse til hjemplaneten Zeron
+    M_s = system.star_mass              # Massen til Stellaris Skarsgard
+    M_zeron = system.masses[0]          # Massen til Zeron
+    period_zeron = np.sqrt((4*np.pi**2 * a_zeron**3)/(astconst.G_sol*(M_s+M_zeron)))
 
-# Perioden til Zeron
-period_zeron = np.sqrt((4*np.pi**2 * a_zeron**3)/(astconst.G_sol*(M_s+M_zeron)))
+    return sim_launch(nperiods*period_zeron, launch_angle=launch_angle)
 
-# the time for the start of the launch relative to zerons period
-launch_time = 1/3
-# the angle relative to the x-axis determining the launch position on the planet
-launch_angle = 2*np.pi*launch_time
-launch_angle = np.pi/2
+# set a positive launch angle for something else than directly away from the sun
+# launch time in measured in years
+def sim_launch(launch_time, launch_angle=-1):
+    # Perioden til Zeron
+    a_zeron = system.semi_major_axes[0] # Store halvakse til hjemplaneten Zeron
+    M_s = system.star_mass              # Massen til Stellaris Skarsgard
+    M_zeron = system.masses[0]          # Massen til Zeron
+    period_zeron = np.sqrt((4*np.pi**2 * a_zeron**3)/(astconst.G_sol*(M_s+M_zeron)))
 
-r1_norm = np.load('rocket_position.npy')
-planet_positions = np.load('positions.npy')
-planet_velocities = np.load('velocities.npy')
+    # the angle relative to the x-axis determining the launch position on the planet
+    # kan be specified to be something else, here it is in the direction straight away from the sun
+    if launch_angle < 0:
+        launch_angle = 2*np.pi*launch_time/period_zeron
 
-# Rotasjonshastighet til Zeron
-omega = 2*np.pi/(system.rotational_periods[0]*60*60*24)
+    r1_norm = np.load('rocket_position.npy')
+    planet_positions = np.load('positions.npy')
+    planet_velocities = np.load('velocities.npy')
 
-dt = 1e-4
-i = int(period_zeron * launch_time/dt)
-r0 = np.zeros((len(r1_norm),2))
-r0[0,0] = utils.AU_to_m(planet_positions[0,0,i])
-r0[0,1] = utils.AU_to_m(planet_positions[1,0,i])
+    # Rotasjonshastighet til Zeron
+    omega = 2*np.pi/(system.rotational_periods[0]*60*60*24)
 
-v0 = np.zeros((len(r1_norm),2))
-v0[0,0] = utils.AU_pr_yr_to_m_pr_s(planet_velocities[0,0,i])
-v0[0,1] = utils.AU_pr_yr_to_m_pr_s(planet_velocities[1,0,i])
+    dt = 1e-4
+    i = int(launch_time/dt)
+    r0 = np.zeros((len(r1_norm),2))
+    r0[0,0] = utils.AU_to_m(planet_positions[0,0,i])
+    r0[0,1] = utils.AU_to_m(planet_positions[1,0,i])
 
-# Posisjonen til raketten relativt til Zeron
-r1 = np.zeros((len(r1_norm),2))
-r1[0] = np.array([r1_norm[0]*np.cos(launch_angle), r1_norm[i]*np.sin(launch_angle)])
+    v0 = np.zeros((len(r1_norm),2))
+    v0[0,0] = utils.AU_pr_yr_to_m_pr_s(planet_velocities[0,0,i])
+    v0[0,1] = utils.AU_pr_yr_to_m_pr_s(planet_velocities[1,0,i])
 
-# Tid
-t = 0
-dt = 1e-3 # s
+    # Posisjonen til raketten relativt til Zeron
+    r1 = np.zeros((len(r1_norm),2))
+    r1[0] = r1_norm[0]*np.cos(launch_angle), r1_norm[i]*np.sin(launch_angle)
 
-# Massen til stjerna Stellaris Skarsgaard
-M = system.star_mass*astconst.m_sun
+    # Tid
+    t = 0
+    dt = 1e-3 # s
+
+    # Massen til stjerna Stellaris Skarsgaard
+    M = system.star_mass*astconst.m_sun
 
 
-for i in range(len(r1_norm)-1):
+    for i in range(len(r1_norm)-1):
 
-    # Regner r1 komponenten til posisjonen
-    # legger til ønsket vinkel definert fra x-aksen til launch-posisjonen
-    r1[i+1] = np.array([r1_norm[i+1]*np.cos(launch_angle + omega*t), r1_norm[i+1]*np.sin(launch_angle + omega*t)])
+        # Regner r1 komponenten til posisjonen
+        # legger til ønsket vinkel definert fra x-aksen til launch-posisjonen
+        r1[i+1] = np.array([r1_norm[i+1]*np.cos(launch_angle + omega*t), r1_norm[i+1]*np.sin(launch_angle + omega*t)])
 
-    # Numerisk integrasjon
-    # Akselerasjonen til Zeron
-    a = - cs.G*M*r0[i]/np.linalg.norm(r0[i])**3
-    v0[i+1] = v0[i]+a*dt
-    r0[i+1] = r0[i]+v0[i+1]*dt # Euler-Cromer
+        # Numerisk integrasjon
+        # Akselerasjonen til Zeron
+        a = - cs.G*M*r0[i]/np.linalg.norm(r0[i])**3
+        v0[i+1] = v0[i]+a*dt
+        r0[i+1] = r0[i]+v0[i+1]*dt # Euler-Cromer
 
-    t += dt
+        t += dt
 
-# Konvertere til AU
-r = utils.m_to_AU(r0+r1)
-r0 = utils.m_to_AU(r0)
+    # Konvertere til AU
+    r = utils.m_to_AU(r0+r1)
+    r0 = utils.m_to_AU(r0)
 
-# Slutthastighet i AU/yr
-vf = (r[-1]-r[-2])/utils.s_to_yr(dt)
+    # Slutthastighet i AU/yr
+    vf = (r[-1]-r[-2])/utils.s_to_yr(dt)
 
+    return r, vf, r0
+
+def plot_sim(r, vf, r0):
+    plt.suptitle('Banene fulgt av raketten og Zeron i det inertielle referansesytemet')
+    plt.xlabel('x [AU]')
+    plt.ylabel('y [AU]')
+    plt.plot(r0[:,0], r0[:,1], 'k:', label='Banen til Zeron')
+    plt.plot(r[:,0], r[:,1], 'k', label='Banen til raketten')
+    plt.scatter(r[-1,0],r[-1,1])
+    plt.scatter(r0[-1,0],r0[-1,1])
+
+#r, vf, r0 = sim_launch_relative_period(2.3)
+r, vf, r0 = sim_launch(4.1)
+plot_sim(r, vf, r0)
 print(f'Final velocity rocket: {vf} AU/yr')
 print(f'Final position rocket: {r[-1]} AU')
 
-plt.suptitle('Banene fulgt av raketten og Zeron i det inertielle referansesytemet')
-plt.xlabel('x [AU]')
-plt.ylabel('y [AU]')
-plt.plot(r0[:,0], r0[:,1], 'k:', label='Banen til Zeron')
-plt.plot(r[:,0], r[:,1], 'k', label='Banen til raketten')
-plt.scatter(0,0)
-plt.scatter(r[-1,0],r[-1,1])
-plt.scatter(r0[-1,0],r0[-1,1])
 
+plt.scatter(0,0)
 plt.axis("equal")
 plt.legend()
 plt.tight_layout()
 
 plt.show()
-#"""
