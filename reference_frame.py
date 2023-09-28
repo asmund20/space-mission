fixed wrong sign

reference_frame.py
@@ -6,12 +6,15 @@ from matplotlib import pyplot as plt, patches
 import numpy as np
 import scipy.constants as cs
 from ast2000tools.solar_system import SolarSystem
+from ast2000tools.space_mission import SpaceMission
 import ast2000tools.utils as utils
 import ast2000tools.constants as astconst
+from simulate_launch import launch
 
 seed = 59529
 
 system = SolarSystem(seed)
+mission = SpaceMission(seed)
 
 #launch time relative to one period for Zeron
 # set a positive launch angle for something else than directly away from the sun
@@ -99,16 +102,27 @@ def plot_sim(r, vf, r0):
     plt.scatter(r[-1,0],r[-1,1])
     plt.scatter(r0[-1,0],r0[-1,1])
 
-#r, vf, r0 = sim_launch_relative_period(2.3)
-r, vf, r0 = sim_launch(4.1)
-plot_sim(r, vf, r0)
-print(f'Final velocity rocket: {vf} AU/yr')
-print(f'Final position rocket: {r[-1]} AU')
+def plotting_og_slikt():
+    #r, vf, r0 = sim_launch_relative_period(2.3)
+    r, vf, r0 = sim_launch(4.1)
+    plot_sim(r, vf, r0)
+    print(f'Final velocity rocket: {vf} AU/yr')
+    print(f'Final position rocket: {r[-1]} AU')
 
 
-plt.scatter(0,0)
-plt.axis("equal")
-plt.legend()
-plt.tight_layout()
+    plt.scatter(0,0)
+    plt.axis("equal")
+    plt.legend()
+    plt.tight_layout()
 
-plt.show()
+    plt.show()
+def ast_test():
+    launch_time = 3
+    dt, z, vz, az, mass, fuel, esc_vel, fuel_consumption, thrust = launch()
+    r, v, r0 = sim_launch(launch_time)
+
+    mission.set_launch_parameters(thrust, fuel_consumption, fuel[0], dt*len(mass), r[0], launch_time)
+    mission.launch_rocket()
+    mission.verify_launch_result(r[-1])
+if __name__ == "__main__":
+    ast_test()

simulate_launch.py
@@ -142,38 +142,43 @@ def simulate_launch(N, fuel_mass, n_boxes):
 
         i += 1
 
-    return dt, z[:i+1], vz[:i+1], az[:i+1], mass[:i+1], fuel[:i+1], esc_vel[:i+1], fuel_consumption
+    return dt, z[:i+1], vz[:i+1], az[:i+1], mass[:i+1], fuel[:i+1], esc_vel[:i+1], fuel_consumption, thrust
 
 
-dt, z, vz, az, mass, fuel, esc_vel, fuel_consumption = simulate_launch(1000, 12000, 2.7e16)
-np.save('rocket_position', z)
+def launch():
+    return  simulate_launch(1000, 12000, 2.7e16)
 
-time = np.linspace(0, dt*(len(z)-1), len(z))
-fig, axs = plt.subplots(3)
-fig.suptitle('Simulering av rakettoppskytning', fontweight='bold')
 
-axs[0].plot(time, z, 'r-', label='Avstand fra sentrum til raketten')
-axs[0].set_ylabel('Avstand [m]')
-axs[0].set_xlabel('Tid [s]')
-axs[0].legend()
+if __name__ == "__main__":
+    dt, z, vz, az, mass, fuel, esc_vel, fuel_consumption, thrust = launch()
+    np.save('rocket_position', z)
 
-axs[1].plot(time, vz, 'g-', label='Fart til raketten')
-axs[1].plot(time, esc_vel, 'k:', label='Unnsplipningshastighet')
-axs[1].set_ylabel('Fart [m/s]')
-axs[1].set_xlabel('Tid [s]')
-axs[1].legend()
+    time = np.linspace(0, dt*(len(z)-1), len(z))
+    fig, axs = plt.subplots(3)
+    fig.suptitle('Simulering av rakettoppskytning', fontweight='bold')
 
-axs[2].plot(time, az, 'b-', label='Akselerasjonen til raketten')
-axs[2].set_ylabel('Akselerasjon [m/s^2]')
-axs[2].set_xlabel('Tid [s]')
-axs[2].legend()
+    axs[0].plot(time, z, 'r-', label='Avstand fra sentrum til raketten')
+    axs[0].set_ylabel('Avstand [m]')
+    axs[0].set_xlabel('Tid [s]')
+    axs[0].legend()
 
-plt.figure()
-plt.plot(time, mass, color='orange', label=f"forbruk: {fuel_consumption:.1f} kg/s")
-plt.ylabel("drivstoff [kg]")
-plt.xlabel("tid [s]")
-plt.suptitle(f"Drivstoff i tanken\ngjenværende drivstoff: {fuel[-1]:.1f} kg")
-plt.legend()
+    axs[1].plot(time, vz, 'g-', label='Fart til raketten')
+    axs[1].plot(time, esc_vel, 'k:', label='Unnsplipningshastighet')
+    axs[1].set_ylabel('Fart [m/s]')
+    axs[1].set_xlabel('Tid [s]')
+    axs[1].legend()
 
-plt.tight_layout()
-plt.show()
+    axs[2].plot(time, az, 'b-', label='Akselerasjonen til raketten')
+    axs[2].set_ylabel('Akselerasjon [m/s^2]')
+    axs[2].set_xlabel('Tid [s]')
+    axs[2].legend()
+
+    plt.figure()
+    plt.plot(time, mass, color='orange', label=f"forbruk: {fuel_consumption:.1f} kg/s")
+    plt.ylabel("drivstoff [kg]")
+    plt.xlabel("tid [s]")
+    plt.suptitle(f"Drivstoff i tanken\ngjenværende drivstoff: {fuel[-1]:.1f} kg")
+    plt.legend()
+
+    plt.tight_layout()
+    plt.show()

spacecraft_orientation.py
@@ -4,22 +4,24 @@ import ast2000tools.constants as cs
 import matplotlib.pyplot as plt
 import numpy as np
 from ast2000tools.space_mission import SpaceMission
+from reference_frame import sim_launch
+from simulate_launch import launch
 
 seed = 59529
 system = SolarSystem(seed)
 mission = SpaceMission(seed)
 
 def doppler_vel(dlamba):
-    return cs.c*dlamba/656.3e-9
+    return -cs.c_AU_pr_yr*dlamba/656.3
 
 def vel_sun():
-    return doppler_vel(np.asarray(mission.star_doppler_shifts_at_sun)*1e-9)
+    return doppler_vel(np.asarray(mission.star_doppler_shifts_at_sun))
 
 def vel_planet(dlambda1, dlambda2):
     return doppler_vel(np.asarray([dlambda1, dlambda2]))
 
 def rel_vel_planet(dlambda1, dlambda2):
-    return vel_planet(dlambda1, dlambda2) - vel_sun()
+    return vel_planet(dlambda1, dlambda2)-vel_sun()
 
 def rel_vel_planet_xy(dlambda1, dlambda2):
     phi = mission.star_direction_angles
@@ -32,19 +34,32 @@ def rel_vel_planet_xy(dlambda1, dlambda2):
 # d has the shape (d_0, d_1, ..., d_n, d_star)
 def triliteration(t: float, d):
     # shape (2, n_planets, n_steps)
-    planet_positions = np.load("position.npy")
+    planet_positions = np.load("positions.npy")
     i = int(t/1e-4)
 
     phi = np.linspace(0, 2*np.pi, 1000, endpoint=False)
 
-    # a sircle around Zeron with radiu equal to the distance to Zeron
-    S = np.asarray([d[0]*np.cos(phi),d[0]*np.sin(phi)]) + d[:,0,i]
+    # a circle around Zeron with radiu equal to the distance to Zeron
+    S = np.asarray([d[0]*np.cos(phi),d[0]*np.sin(phi)])
+    S[0] += planet_positions[0,0,i]
+    S[1] += planet_positions[1,0,i]
 
-    # the sum of the square errors for the distance to the two other planets
-    e = (S-d[:,1,i])**2+(S-d[:,2,i])**2
+    # the sum of the square errors for the distance to the rest of the planets
+    e = S.copy()
+    for j, dj in enumerate(d[1:]):
+        A = S.copy()
+        A[0,:] = S[0,:]-planet_positions[0,j,i]
+        A[1,:] = S[1,:]-planet_positions[1,j,i]
 
-    i = np.argmin(e)[0]
-    return S[i]
+        for k, a in enumerate(A):
+            A[k] = np.linalg.norm(a)
+
+        A -= dj
+
+        e += A**2
+
+    i = np.argmin(e)
+    return S[:,i]
 
 
 def main():
@@ -58,5 +73,18 @@ def main():
     l2 = l2 *1e-9
     print(rel_vel_planet_xy(l1, l2))
 
+    launch_time = 3
+    dt, z, vz, az, mass, fuel, esc_vel, fuel_consumption, thrust = launch()
+    r, v, r0 = sim_launch(launch_time)
+
+    mission.set_launch_parameters(thrust, fuel_consumption, fuel[0], dt*len(mass), r[0], launch_time)
+    mission.launch_rocket()
+    mission.verify_launch_result(r[-1])
+    dlambda1, dlambda2 = mission.measure_star_doppler_shifts()
+    d = mission.measure_distances()
+    print(rel_vel_planet(dlambda1, dlambda2))
+    print(v)
+    print(triliteration(launch_time + dt*len(mass)/(60**2*365), d))
+    print(r[-1])
 
 main()