b8227a1e16a63793c3779db125992d32c8b02cdd
felixmorland <felmorland@gmail.com>
2023-10-10 14:14:09 +0200

    Update solar_system_info.py

--- solar_system_info.py
+++ solar_system_info.py
@@ -2,6 +2,7 @@ from ast2000tools.solar_system import SolarSystem
 from ast2000tools.space_mission import SpaceMission
 import ast2000tools.constants as cs
 import ast2000tools.utils as ut
+import numpy as np
 mission = SpaceMission(59529)
 system = SolarSystem(59529)
 
@@ -29,3 +30,7 @@ for i, gi in enumerate(g):
     print(f"planet {i} bidrar til gravitasjonsfeltet i sentrum til Stellaris Skarsgård med {gi} ganger en konstant")
 
 print(f'Star angles used for Doppler: phi1 = {mission.star_direction_angles[0]:.6g}, phi2 = {mission.star_direction_angles[1]:.6g}')
+l = system.semi_major_axes[1]*np.sqrt(system.masses[1]/(10*system.star_mass))
+print(f'l = {ut.AU_to_km(l)}')
+print(f'L = {system.radii[1]*640*180/70/np.pi} = {ut.km_to_AU(system.radii[1]*640*180/70/np.pi)}')
+print(f'Tvekne eksentrisitet: {system.eccentricities[1]}')
